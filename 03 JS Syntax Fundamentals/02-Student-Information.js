a691aa9af535f7d2777706f572adcab63bb6c023
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2025-03-03 19:46:09 +0200

    Deleted folder 03 JS Syntax Fundamentals

--- 03 JS Syntax Fundamentals/02-Student-Information.js
+++
@@ -1,8 +0,0 @@
-function studentInfo(name, age, grade) {
-    console.log(`Name: ${name}, Age: ${age}, Grade: ${Number(grade).toFixed(2)}`);
-};
-
-studentInfo('John', 15, 5.54678)
-studentInfo('Steve', 16, 2.1426)
-studentInfo('Marry', 12, 6.00)
-
--- 03 JS Syntax Fundamentals/10-Numbers-from-M-to-N.js
+++
@@ -1,8 +0,0 @@
-function fromMToN(m,n){
-    for (let i=m; i>=n; i--){
-        console.log(i);
-    };
-};
-
-fromMToN(6,2);
-fromMToN(4,1);
