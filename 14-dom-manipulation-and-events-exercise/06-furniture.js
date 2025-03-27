5291a26e49aa757cdea2084ac36145b2151d2cea
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2025-03-27 14:05:51 +0200

    Add DOM manipulation and events exercise solutions

--- 14-dom-manipulation-and-events-exercise/06-furniture.js
+++ 14-dom-manipulation-and-events-exercise/06-furniture.js
@@ -1,44 +1,59 @@
 function solve() {
-    const generateForm = document.getElementById('input');
-    const shopForm = document.getElementById('shop');
-  
-    generateForm.addEventListener('submit', (e) => {
-      e.preventDefault()
-  
-      let inputText = e.target.querySelector('textarea');
-      let inputArr = JSON.parse(inputText.value);
-  
-      inputArr.forEach((product) => {
-        let newTr = document.querySelector('tbody tr').cloneNode(true);
-  
-        newTr.children[0].innerHTML = `<img src="${product.img}">`;
-        newTr.children[1].innerHTML = `<p>${product.name}</p>`;
-        newTr.children[2].innerHTML = `<p>${product.price}</p>`;
-        newTr.children[3].innerHTML = `<p>${product.decFactor}</p>`;
-        newTr.children[4].innerHTML = `<input type="checkbox" />`;
-  
-        document.querySelector('tbody').appendChild(newTr);
-      })
-  
+  const generateForm = document.getElementById('input');
+  const shopForm = document.getElementById('shop');
+  const tableBody = document.querySelector('tbody')
+
+  generateForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let inputText = e.target.querySelector('textarea');
+    let inputArr = JSON.parse(inputText.value);
+
+    inputArr.forEach((product) => {
+      let newTr = document.createElement('tr');
+
+      appendNewEl(newTr, 'img', 'src', product.img);
+      appendNewEl(newTr, 'p', 'textContent', product.name);
+      appendNewEl(newTr, 'p', 'textContent', product.price);
+      appendNewEl(newTr, 'p', 'textContent', product.decFactor);
+      appendNewEl(newTr, 'input', 'type', 'checkbox');
+
+      tableBody.appendChild(newTr);
     })
-  
-    shopForm.addEventListener('submit', (e) => {
-      e.preventDefault()
-  
-      let result = e.target.querySelector('textarea')
-      let furnituresName = [];
-      let totalPrice = 0;
-      let decFactor = 0;
-  
-      Array.from(document.querySelectorAll('tr:has(input[type="checkbox"]:checked)')).forEach((element) => {
-  
-        furnituresName.push(element.querySelector('td:nth-child(2) p').textContent);
-        totalPrice += Number(element.querySelector('td:nth-child(3) p').textContent);
-        decFactor += Number(element.querySelector('td:nth-child(4) p').textContent);
-      })
-  
-      result.textContent = `Bought furniture: ${furnituresName.join(', ')}\n`;
-      result.textContent += `Total price: ${totalPrice}\n`;
-      result.textContent += `Average decoration factor: ${decFactor / furnituresName.length}\n`;
-    })
+  })
+
+  function appendNewEl(parent, element, property, text) {
+    let newTd = document.createElement('td');
+    let newEle = document.createElement(element);
+
+    Object.assign(newEle, { [property]: text });
+
+    newTd.appendChild(newEle);
+    parent.appendChild(newTd);
   }
+
+  shopForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let result = e.target.querySelector('textarea')
+    let furnituresName = [];
+    let totalPrice = 0;
+    let totalDecFactor = 0;
+
+    Array.from(document.querySelectorAll('tr:has(input[type="checkbox"]:checked)')).forEach((element) => {
+
+      let [name, price, DecFactor] = element.querySelectorAll('td:nth-child(n+2) p');
+
+      furnituresName.push(name.textContent);
+      totalPrice += Number(price.textContent);
+      totalDecFactor += Number(DecFactor.textContent);
+    })
+
+    let avgDecFactor = totalDecFactor / furnituresName.length
+
+    result.textContent =
+      `Bought furniture: ${furnituresName.join(', ')}\n` +
+      `Total price: ${totalPrice}\n` +
+      `Average decoration factor: ${avgDecFactor}\n`;
+  })
+}
